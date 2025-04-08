f6a9e904e1c8b07d92bfc2126ed4cc3485debf65
Abhinash01 <abinashgupa111@gmail.com>
2025-04-08 13:02:33 +0530

    DOM Started

---
+++ DOM/dom1.js
@@ -0,0 +1,6 @@
+// 1. What is DOM?
+// A. The Document Object Model (DOM) is an application programming interface (API) to manipulate HTML documents.
+
+// getelementbyId () method ->
+let nm = document.getElementById("myName");
+console.log(nm);
